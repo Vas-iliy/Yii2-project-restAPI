7069227abb230964a52b66239482aec781c33ddc
Vasiliy <kkolyasev1228@gmail.com>
2022-01-27 17:02:01 +0400

    post and article

---
+++ models/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Article extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    public function rules()
+    {
+        return [
+            [['title', 'content'], 'required'],
+        ];
+    }
+}
--- modules/rest/controllers/PostController.php
+++ modules/rest/controllers/PostController.php
@@ -2,17 +2,70 @@
 
 namespace app\modules\rest\controllers;
 
+use app\models\Article;
+use app\modules\admin\models\Post;
 use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\helpers\Url;
 use yii\rest\ActiveController;
+use yii\web\ServerErrorHttpException;
 
 class PostController extends ActiveController
 {
     public $modelClass = 'app\modules\admin\models\Post';
 
+    public function behaviors()
+    {
+        $behaviors  = parent::behaviors();
+        //$behaviors['authenticator']['only'] => ['create', 'update', 'delete']
+        $behaviors['authenticator']['authMethods'] = [
+            HttpBasicAuth::class,
+            HttpBearerAuth::class,
+        ];
+
+        $behaviors['access'] = [
+            'class' => AccessControl::class,
+            //'only' => ['create', 'update', 'delete'],
+            'rules' => [
+                [
+                    'allow' => true,
+                    'roles' => ['@'],
+                ],
+            ],
+        ];
+        return $behaviors;
+    }
+
     public function actions()
     {
         $actions = parent::actions();
-        unset($actions['create'], $actions['update'], $actions['delete']);
+        unset($actions['create']);
         return $actions;
     }
+
+    public function actionCreate()
+    {
+        $model = new Article();
+        $model->user_id = \Yii::$app->user->id;
+        $model->load(\Yii::$app->getRequest()->getBodyParams(), '');
+        if ($model->save()) {
+            $response = \Yii::$app->getResponse();
+            $response->setStatusCode(201);
+            $id = implode(',', $model->getPrimaryKey(true));
+            $response->getHeaders()->set('Location', Url::toRoute(['view', 'id' => $id], true));
+        } elseif (!$model->hasErrors()) {
+            throw new ServerErrorHttpException('Failed to create the object for unknown reason.');
+        }
+
+        return $model;
+    }
+
+    /*public function checkAccess($action, $model = null, $params = [])
+    {
+        if (in_array($action, ['update', 'delete'])) {
+            if (!\Yii::$app->user->can(Rbac::))
+        }
+    }*/
 }
