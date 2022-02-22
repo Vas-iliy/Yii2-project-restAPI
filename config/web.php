41e84d0a162e9b0aa05f3f45319494d5ea4ad4ed
Vasiliy <kkolyasev1228@gmail.com>
2022-02-22 17:47:13 +0400

    refactor

--- config/web.php
+++ config/web.php
@@ -12,6 +12,7 @@ $config = [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
+        '@we'   => '/web',
     ],
     'components' => [
         'request' => [
--- modules/admin/controllers/AuthController.php
+++ modules/admin/controllers/AuthController.php
@@ -14,12 +14,12 @@ class AuthController extends AppAdminController
     public function actionLogin()
     {
         if (!Yii::$app->user->isGuest) {
-            return $this->redirect('/project-restAPI/admin');
+            return $this->redirect('/project-restAPI');
         }
 
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->redirect('/project-restAPI/admin');
+            return $this->redirect('/project-restAPI');
         }
 
         $model->password = '';
@@ -31,18 +31,18 @@ class AuthController extends AppAdminController
     public function actionLogout()
     {
         Yii::$app->user->logout();
-        return $this->redirect('/project-restAPI/admin');
+        return $this->redirect('/project-restAPI');
     }
 
     public function actionSignup()
     {
         if (!Yii::$app->user->isGuest) {
-            return $this->redirect('/project-restAPI/admin');
+            return $this->redirect('/project-restAPI');
         }
 
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
-            return $this->redirect('/project-restAPI/admin');
+            return $this->redirect('/project-restAPI');
         }
 
         return $this->render('signup', compact('model'));
